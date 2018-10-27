a81e6ae9f5148a8e0245b81e57e7e589c5e282f9
Basil Suter <git@nadar.io>
2018-10-27 13:39:54 +0200

    added request body

---
+++ src/spec/MediaType.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Each Media Type Object provides schema and examples for the media type identified by its key.
+ *
+ */
+class MediaType
+{
+
+}
--- src/spec/RequestBody.php
+++ src/spec/RequestBody.php
@@ -9,10 +9,36 @@ use cebe\openapi\SpecBaseObject;
  *
  * @link https://github.com/OAI/OpenAPI-Specification/blob/3.0.2/versions/3.0.2.md#requestBodyObject
  *
+ * @property-read string $description
+ * @property-read array $content
+ * @property-read string $required
+ * 
  * @author Carsten Brandt <mail@cebe.cc>
+ * 
+ * @TODO: Unsure how to define the content attribtue defintion and validation defintion.:
+ * > REQUIRED. The content of the request body. The key is a media type or media type range and the value describes it.
+ * > For requests that match multiple keys, only the most specific key is applicable. e.g. text/plain overrides text/*
  */
 class RequestBody
 {
-    // TODO implement
-
+    /**
+     * @return array array of attributes available in this object.
+     */
+    protected function attributes(): array
+    {
+        return [
+            'description' => Type::STRING,
+            'content' => [Type::STRING, MediaType::class],
+            'required' => Type::BOOLEAN,
+        ];
+    }
+     /**
+     * Perform validation on this object, check data against OpenAPI Specification rules.
+     *
+     * Call `addError()` in case of validation errors.
+     */
+    protected function performValidation()
+    {
+        $this->requireProperties(['content']);
+    }
 }
