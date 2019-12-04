7457351156e4e642d7805e257e114b4495fe19a2
Carsten Brandt <mail@cebe.cc>
2019-12-04 09:12:30 +0100

    add test to verify reading of YAML tags
    make sure it is able to read specs written with traits/mixins as
    discussed in
    https://github.com/OAI/OpenAPI-Specification/issues/613#issuecomment-203673611

--- tests/ReaderTest.php
+++ tests/ReaderTest.php
@@ -36,6 +36,47 @@ YAML
         $this->assertApiContent($openapi);
     }
 
+    /**
+     * Test if reading YAML file with anchors works
+     */
+    public function testReadYamlWithAnchors()
+    {
+        $openApiFile = __DIR__ . '/spec/data/traits-mixins.yaml';
+        $openapi = \cebe\openapi\Reader::readFromYamlFile($openApiFile);
+
+        $this->assertApiContent($openapi);
+
+        $putOperation = $openapi->paths['/foo']->put;
+        $this->assertEquals('create foo', $putOperation->description);
+        $this->assertTrue($putOperation->responses->hasResponse('200'));
+        $this->assertTrue($putOperation->responses->hasResponse('404'));
+        $this->assertTrue($putOperation->responses->hasResponse('428'));
+        $this->assertTrue($putOperation->responses->hasResponse('default'));
+
+        $respOk = $putOperation->responses->getResponse('200');
+        $this->assertEquals('request succeeded', $respOk->description);
+        $this->assertEquals('the request id', $respOk->headers['X-Request-Id']->description);
+
+        $resp404 = $putOperation->responses->getResponse('404');
+        $this->assertEquals('resource not found', $resp404->description);
+        $this->assertEquals('the request id', $resp404->headers['X-Request-Id']->description);
+
+        $resp428 = $putOperation->responses->getResponse('428');
+        $this->assertEquals('resource not found', $resp428->description);
+        $this->assertEquals('the request id', $resp428->headers['X-Request-Id']->description);
+
+        $respDefault = $putOperation->responses->getResponse('default');
+        $this->assertEquals('resource not found', $respDefault->description);
+        $this->assertEquals('the request id', $respDefault->headers['X-Request-Id']->description);
+
+        $foo = $openapi->components->schemas['Foo'];
+        $this->assertArrayHasKey('uuid', $foo->properties);
+        $this->assertArrayHasKey('name', $foo->properties);
+        $this->assertArrayHasKey('id', $foo->properties);
+        $this->assertArrayHasKey('description', $foo->properties);
+        $this->assertEquals('uuid of the resource', $foo->properties['uuid']->description);
+    }
+
     private function assertApiContent(\cebe\openapi\spec\OpenApi $openapi)
     {
         $result = $openapi->validate();
