b4a47f4869522655274e593bd028a47c417312a5
Carsten Brandt <mail@cebe.cc>
2019-12-04 11:34:09 +0100

    ignore deprecation warning in PHP 7.4
    https://github.com/symfony/symfony/issues/34807

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
-error_reporting(-1);
+if (PHP_MAJOR_VERSION == 7 && PHP_MINOR_VERSION == 4) {
+    // ignore deprecation warning in PHP 7.4
+    // https://github.com/symfony/symfony/issues/34807
+    error_reporting(-1 & ~E_DEPRECATED);
+} else {
+    error_reporting(-1);
+}
 
 require __DIR__ . '/../vendor/autoload.php';
