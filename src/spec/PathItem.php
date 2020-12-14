56875ddf59d46e21ac4176feafa9aeaa99905dbb
rv1971 <rv1971@web.de>
2020-12-14 16:59:25 +0100

    Fix PathItem::resolveReferences() for array fields
    The previous code `$this->$attribute[$k] = $referencedObject` had
    no effect  if the value of `$attribute` is an array (as for instance
    in the case of `parameters`), because `$this->$attribute` invokes
    `SpecBaseObject::__get()`, which does *not* return a reference. Indeed
    PHP issued `Notice: Indirect modification of overloaded property
    cebe\openapi\spec\PathItem::$parameters has no effect` in this case.

--- src/spec/PathItem.php
+++ src/spec/PathItem.php
@@ -174,7 +174,8 @@ class PathItem extends SpecBaseObject
                     foreach ($this->$attribute as $k => $item) {
                         if ($item instanceof Reference) {
                             $referencedObject = $item->resolve();
-                            $this->$attribute[$k] = $referencedObject;
+                            $this->$attribute =
+                              [ $k => $referencedObject ] + $this->$attribute;
                             if (!$referencedObject instanceof Reference && $referencedObject !== null) {
                                 $referencedObject->resolveReferences();
                             }
