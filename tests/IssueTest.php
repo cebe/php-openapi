5457f659c824f5cddf3cd0f08b0cfd4cd236a80f
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2025-01-09 20:16:48 +0530

    WIP

--- tests/IssueTest.php
+++ tests/IssueTest.php
@@ -7,7 +7,15 @@ class IssueTest extends \PHPUnit\Framework\TestCase
     // https://github.com/cebe/php-openapi/issues/165
     public function test165WrongErrorMessageWhenUsingAnUndefinedPropertyForASchemaObjectPropertyDefinition()
     {
-        $openapi = Reader::readFromYamlFile(__DIR__.'/data/issue/165/spec.yml');
-        $this->assertTrue($openapi->validate());
+//        $openapi = Reader::readFromYamlFile(__DIR__.'/data/issue/165/spec.yml');
+//        $openapi->performValidation();
+//        $this->assertTrue($openapi->getErrors());
+//        $this->assertTrue($openapi->validate());
+
+
+//        exec('pwd', $output, $code);
+        exec('bin/php-openapi validate tests/data/issue/165/spec.yml', $output, $code);
+        $this->assertSame($output, ['/path/to/php-openapi']);
+        // $this->assertSame(0, $code);
     }
 }
