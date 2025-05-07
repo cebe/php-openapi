893ab104be1f5dfe5a39766703f583584e43c6e1
Carsten Brandt <mail@cebe.cc>
2025-05-07 10:44:12 +0200

    code style fixes

--- src/ReferenceContext.php
+++ src/ReferenceContext.php
@@ -125,11 +125,11 @@ class ReferenceContext
             if ($i > 0 && $parts[$i] === '..') {
                 $parent = $i - $parentOffset;
                 //Make sure parent exists, if not, check the next parent etc
-                while($parent >= 0 && empty($parts[$parent])){
-                        $parent--;
+                while ($parent >= 0 && empty($parts[$parent])) {
+                    $parent--;
                 }
                 //Confirm parent is valid
-                if(!empty($parts[$parent]) && $parts[$parent] !== '..'){
+                if (!empty($parts[$parent]) && $parts[$parent] !== '..') {
                     unset($parts[$parent]);
                 }
                 unset($parts[$i]);
