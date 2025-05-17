19fabbc08e99e1f88e4b0c161e59fab742a7e084
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2025-05-17 13:36:02 +0530

    Add more tests

--- tests/issues/242/Issue242Test.php
+++ tests/issues/242/Issue242Test.php
@@ -2,6 +2,7 @@
 
 use cebe\openapi\Reader;
 use cebe\openapi\SpecObjectInterface;
+use cebe\openapi\Writer;
 use PHPUnit\Framework\TestCase;
 
 // https://github.com/cebe/php-openapi/issues/242
@@ -26,10 +27,11 @@ class Issue242Test extends TestCase
         return preg_replace('/\e\[[0-9;]*m/', '', $string);
     }
 
-    public function test242Case2()
+    public function test242Case2() # https://github.com/cebe/php-openapi/issues/242#issuecomment-2886431173
     {
         // read in yml
-        $openapi = Reader::readFromYamlFile(dirname(__DIR__, 2) . '/data/issue/242/spec2.yml');
+        $file = dirname(__DIR__, 2) . '/data/issue/242/spec2.yml';
+        $openapi = Reader::readFromYamlFile($file);
         $this->assertInstanceOf(SpecObjectInterface::class, $openapi);
         $this->assertSame(json_decode(json_encode($openapi->paths['/endpoint']->get->security->getSerializableData()), true), [
             [
@@ -38,8 +40,106 @@ class Issue242Test extends TestCase
             ]
         ]);
 
-        // write in yml # TODO
-        // read in json # TODO
-        // write in json # TODO
+        # write back to yml
+        $json = Writer::writeToYaml($openapi);
+        $this->assertEquals(preg_replace('~\R~', "\n", <<<YAML
+openapi: 3.0.0
+info:
+  title: 'API Documentation'
+  description: 'All API endpoints are presented here.'
+  version: 1.0.0
+servers:
+  -
+    url: 'http://127.0.0.1:8080/'
+paths:
+  /endpoint:
+    get:
+      responses:
+        '200':
+          description: OK
+      security:
+        -
+          apiKey: []
+          bearerAuth: []
+components:
+  securitySchemes:
+    apiKey:
+      type: apiKey
+      name: X-APi-Key
+      in: header
+    bearerAuth:
+      type: http
+      description: 'JWT Authorization header using the Bearer scheme.'
+      scheme: bearer
+      bearerFormat: JWT
+
+YAML
+        ),
+            $json
+        );
+
+        // read in json
+        $file = dirname(__DIR__, 2) . '/data/issue/242/spec2.json';
+        $openapi = Reader::readFromJsonFile($file);
+        $this->assertInstanceOf(SpecObjectInterface::class, $openapi);
+        $this->assertSame(json_decode(json_encode($openapi->paths['/endpoint']->get->security->getSerializableData()), true), [
+            [
+                'apiKey' => [],
+                'bearerAuth' => []
+            ]
+        ]);
+
+        // write back in json
+        $json = Writer::writeToJson($openapi);
+        $this->assertEquals(preg_replace('~\R~', "\n", <<<JSON
+{
+    "openapi": "3.0.0",
+    "info": {
+        "title": "API Documentation",
+        "description": "All API endpoints are presented here.",
+        "version": "1.0.0"
+    },
+    "servers": [
+        {
+            "url": "http:\/\/127.0.0.1:8080\/"
+        }
+    ],
+    "paths": {
+        "\/endpoint": {
+            "get": {
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    }
+                },
+                "security": [
+                    {
+                        "apiKey": [],
+                        "bearerAuth": []
+                    }
+                ]
+            }
+        }
+    },
+    "components": {
+        "securitySchemes": {
+            "apiKey": {
+                "type": "apiKey",
+                "name": "X-APi-Key",
+                "in": "header"
+            },
+            "bearerAuth": {
+                "type": "http",
+                "description": "JWT Authorization header using the Bearer scheme.",
+                "scheme": "bearer",
+                "bearerFormat": "JWT"
+            }
+        }
+    }
+}
+JSON
+        ),
+            $json
+        );
     }
 }
